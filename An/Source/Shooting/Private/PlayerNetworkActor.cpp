b42f66e6652e15176a535b75569758fe063dd379
An Dae Hun <dkseogns88@naver.com>
2024-02-17 04:48:03 +0900

    Update for server connection

--- An/Source/Shooting/Private/PlayerNetworkActor.cpp
+++ An/Source/Shooting/Private/PlayerNetworkActor.cpp
@@ -3,13 +3,21 @@
 
 #include "PlayerNetworkActor.h"
 #include "Net/UnrealNetwork.h"
+#include "Components/SkeletalMeshComponent.h"
 // Sets default values
 APlayerNetworkActor::APlayerNetworkActor()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	// 네트워크 활성화
+	// 네트워크 활성화 및 기타 설정
 	SetReplicates(true);
 	PrimaryActorTick.bCanEverTick = true;
+
+	// 스켈레탈 메시 컴포넌트 생성 및 설정
+	GetMesh()->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, -90.0f), FRotator(0.0f, -90.0f, 0.0f));
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> MeshAsset(TEXT("SkeletalMesh'/Game/Path/To/Your/Mesh.Mesh'"));
+	if (MeshAsset.Succeeded())
+	{
+		GetMesh()->SetSkeletalMesh(MeshAsset.Object);
+	}
 
 }
 
@@ -47,5 +55,41 @@ void APlayerNetworkActor::SetupPlayerInputComponent(UInputComponent* PlayerInput
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	// 이동 입력 바인딩
+	PlayerInputComponent->BindAxis("MoveForward", this, &APlayerNetworkActor::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &APlayerNetworkActor::MoveRight);
+
+	// 회전 입력 바인딩
+	PlayerInputComponent->BindAxis("Turn", this, &APlayerNetworkActor::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APlayerNetworkActor::AddControllerPitchInput);
 }
+
+void APlayerNetworkActor::MoveForward(float Value)
+{
+	if ((Controller != nullptr) && (Value != 0.0f))
+	{
+		// 이동 방향 결정
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// 전/후 이동 벡터 계산
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void APlayerNetworkActor::MoveRight(float Value)
+{
+	if ((Controller != nullptr) && (Value != 0.0f))
+	{
+		// 이동 방향 결정
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// 좌/우 이동 벡터 계산
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(Direction, Value);
+	}
+}
+
 
--- An/Source/Shooting/Public/PlayerNetworkActor.h
+++ An/Source/Shooting/Public/PlayerNetworkActor.h
@@ -35,4 +35,9 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	void MoveForward(float Value);
+
+	void MoveRight(float Value);
+
+	
 };
